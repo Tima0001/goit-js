068b8b2e8384e258274bb7b862e7f339ff0cf2fb
Tima0001 <microsoft.timo1@gmail.com>
2020-02-29 16:47:31 +0200

    classwor done

---
+++ Homework-1/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+let a;
+console.log(a);
+console.log(typeof a);
+
+a = 34;
+console.log(a);
+console.log(typeof a);
+
+let b = "JavaScript";
+b = 30
+console.log(b);
+console.log(typeof b);
+
+let c = false;
+c = true
+console.log(c);
+console.log(typeof c);
+
+var d = 20;
+console.log(d);
+console.log(typeof d);
+
+const name = "Timo";
+console.log(name);
+console.log(typeof name);
+// name = "Kate";
+// console.log(name);
+// console.log(typeof name);
+
+const isAuthenticad = true;
+console.log(isAuthenticad);
+console.log(typeof isAuthenticad);
+
+const isModalOpen = false;
+console.log(isModalOpen);
+console.log(typeof isModalOpen);
+
+const fe_group = null;
+console.log(fe_group);
+console.log(typeof fe_group);
+
+const message = "Жопа!"
+console.log(message);
+
+let men = "Ben"
+console.log(men);
+console.log(`${men}, ${message}`);
+
+
+// alert(`${men}, ${message}`);
+
+// const isReady = confirm("Are you ready?");
+// console.log(isReady);
+
+const enterName = prompt("Enter your name")
+console.log(enterName);
+console.log(typeof null)
