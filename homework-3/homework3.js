9dbd4194d920c152ae8a23272c4d9d77f528436d
Tima0001 <microsoft.timo1@gmail.com>
2020-06-03 12:07:02 +0300

    fix module3

--- homework-3/homework3.js
+++ homework-3/homework3.js
@@ -1,60 +1,66 @@
-"use strict";
+// "use strict";
 
-///// 1 /////
+// ///// 1 /////
 
-const user = {
-    name: "Semen",
-    age: 15,
-    hobby: "html",
-    premium: true,
-};
+// const user = {
+//     name: "Semen",
+//     age: 15,
+//     hobby: "html",
+//     premium: true,
+// };
 
-const toShowUpdatedUser = function (obj) {
-    user.mood = "happy";
-    user.hobby = "coding";
-    user.premium = "false";
-};
+// const toShowUpdatedUser = function (obj) {
+//     obj.mood = "happy";
+//     obj.hobby = "coding";
+//     obj.premium = false;
 
-toShowUpdatedUser(user);
-console.log(user);
+//     const keys = Object.keys(obj);
+//     const values = Object.values(obj);
 
-///// 2 /////
+//     for (let i = 0; i < keys.length; i++) {
+//         console.log(`${keys[i]}: ${values[i]}`);
+//     }
+// };
+// toShowUpdatedUser(user);
 
-const countProps = function (obj) {
-    return Object.keys(obj).length;
-};
 
-console.log(countProps({})); // 0
+// ///// 2 /////
 
-console.log(countProps({ name: "Mango", age: 2 })); // 2
+// const countProps = function (obj) {
+//     return Object.keys(obj).length;
+// };
 
-console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
+// console.log(countProps({})); // 0
 
-///// 3 /////
+// console.log(countProps({ name: "Mango", age: 2 })); // 2
 
-const countTotalSalary = function (employees) {
-    let employer = Object.values(employees);
-    let total = 0;
-    for (let val of employer) {
-        total += val;
-    };
-    console.log(total);
-};
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
 
-console.log(
-    countTotalSalary({
-        mango: 100,
-        poly: 150,
-        alfred: 80,
-    })
-); // 330
-console.log(
-    countTotalSalary({
-        kiwi: 200,
-        lux: 50,
-        chelsy: 150,
-    })
-); // 400
+// ///// 3 /////
+
+// function countTotalSalary(employees) {
+//     let employer = Object.values(employees);
+//     let sum = 0;
+//     for (let val of employer) {
+//         sum += val;
+//     }
+//     return sum;
+// };
+
+// console.log(
+//     countTotalSalary({
+//         mango: 100,
+//         poly: 150,
+//         alfred: 80,
+//     })
+// ); // 330
+// console.log(
+//     countTotalSalary({
+//         kiwi: 200,
+//         lux: 50,
+//         chelsy: 150,
+//     })
+// ); // 400
 
 ///// 4 /////
 
@@ -67,11 +73,10 @@ const products = [
 
 const calculateTotalPrice = function (Allproducts, productName) {
     for (let oneProduct of Allproducts) {
-        if (oneProduct.name === productName) {
-            return console.log(oneProduct.price * oneProduct.quantity);
-        };
-    };
+        if (oneProduct.name === productName)
+            return oneProduct.price * oneProduct.quantity;
+    }
 };
 
-console.log(calculateTotalPrice(products, "Радар")); 
+console.log(calculateTotalPrice(products, "Радар"));
 console.log(calculateTotalPrice(products, "Дроид")); 
